9d43697ce563cc725fb7e4aafa04e9ea69ae47d1
kpcyrd <git@rxv.cc>
2017-10-27 20:54:06 +0200

    Add vuln-chroot challenge

---
+++ examples/vuln-chroot.rs
@@ -0,0 +1,43 @@
+extern crate boxxy;
+extern crate env_logger;
+extern crate libc;
+extern crate errno;
+
+use std::env;
+use std::ffi::CString;
+
+fn chroot(path: &str) -> Result<(), errno::Errno> {
+    let path = CString::new(path).unwrap();
+    let ret = unsafe { libc::chroot(path.as_ptr()) };
+
+    if ret == 0 {
+        Ok(())
+    } else {
+        Err(errno::errno())
+    }
+}
+
+fn getuid() -> libc::uid_t {
+    unsafe { libc::getuid() }
+}
+
+fn main() {
+    if getuid() != 0 {
+        println!("Error: this challenge needs root to set up");
+        println!("\tcargo build --examples && sudo target/debug/examples/vuln-chroot");
+        return;
+    }
+
+    let jail = "jails/empty/";
+    println!("[*] creating jail at {:?}", jail);
+    std::fs::create_dir_all(&jail).unwrap();
+
+    println!("[*] starting chroot...");
+    chroot(&jail).unwrap();
+
+    env::set_current_dir("/").unwrap();
+
+    println!("[+] jail is active, can you escape it?");
+
+    boxxy::Shell::new(boxxy::Toolbox::new()).run()
+}
