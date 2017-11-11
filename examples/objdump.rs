86fcf62c5c58a39eed41a9ba0e6ebd0aa17e7b7c
kpcyrd <git@rxv.cc>
2017-11-11 17:44:12 +0100

    Cleanup objdump.rs

--- examples/objdump.rs
+++ examples/objdump.rs
@@ -4,6 +4,21 @@ extern crate clap;
 
 use clap::{App, Arg, AppSettings};
 
+#[derive(Debug)]
+enum Encoding {
+    Hex,
+    Base64,
+}
+
+#[inline]
+fn encode(data: &[u8], encoding: Encoding) -> String {
+    match encoding {
+        Encoding::Hex => data.iter()
+                .fold(String::new(), |a, b| a + &format!("\\x{:02x}", b)),
+        Encoding::Base64 => base64::encode(data),
+    }
+}
+
 fn main() {
     let matches = App::new("objdump")
         .setting(AppSettings::DisableVersion)
@@ -18,18 +33,15 @@ fn main() {
         .get_matches();
 
     let path = matches.value_of("path").unwrap();
-    let hex = matches.occurrences_of("hex") > 0;
+
+    let encoding = match matches.occurrences_of("hex") > 0 {
+        true => Encoding::Hex,
+        false => Encoding::Base64,
+    };
 
     let file = elf::File::open_path(path).expect("couldn't open file");
     let section = file.get_section(".text").expect("couldn't find .text section");
 
-    let shellcode = {
-        if hex {
-            section.data.iter()
-                .fold(String::new(), |a, b| a + &format!("\\x{:02x}", b))
-        } else {
-            base64::encode(&section.data)
-        }
-    };
+    let shellcode = encode(&section.data, encoding);
     println!("{}", shellcode);
 }
