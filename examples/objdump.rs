d181c9d245f2f4fe8d93dd0555d10d282feb9e59
kpcyrd <git@rxv.cc>
2017-11-11 03:41:01 +0100

    Output hex with objdump -x

--- examples/objdump.rs
+++ examples/objdump.rs
@@ -1,14 +1,35 @@
 extern crate elf;
 extern crate base64;
+extern crate clap;
 
-use std::env;
+use clap::{App, Arg, AppSettings};
 
 fn main() {
-    let path = env::args().nth(1).expect("couldn't find path argument");
+    let matches = App::new("objdump")
+        .setting(AppSettings::DisableVersion)
+        .arg(Arg::with_name("hex")
+            .help("use hex encoding")
+            .short("x")
+        )
+        .arg(Arg::with_name("path")
+            .help("binary path")
+            .required(true)
+        )
+        .get_matches();
+
+    let path = matches.value_of("path").unwrap();
+    let hex = matches.occurrences_of("hex") > 0;
 
     let file = elf::File::open_path(path).expect("couldn't open file");
     let section = file.get_section(".text").expect("couldn't find .text section");
 
-    let shellcode = base64::encode(&section.data);
+    let shellcode = {
+        if hex {
+            section.data.iter()
+                .fold(String::new(), |a, b| a + &format!("\\x{:02x}", b))
+        } else {
+            base64::encode(&section.data)
+        }
+    };
     println!("{}", shellcode);
 }
