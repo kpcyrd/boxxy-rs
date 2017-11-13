a32371ea7f71702c7e98e7adfed78f0bc871a079
kpcyrd <git@rxv.cc>
2017-11-13 20:06:09 +0100

    Level-up travis

---
+++ ci/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+
+case "$TRAVIS_OS_NAME-$TARGET" in
+    linux-x86_64-unknown-linux-gnu|osx-x86_64-apple-darwin)
+        cargo test --verbose
+        ;;
+    *)
+        # nop
+        ;;
+esac
