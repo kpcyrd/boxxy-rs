48cfb43135c9c77d94f41c9499a30a8b4f3f60c8
kpcyrd <git@rxv.cc>
2018-05-12 01:46:38 +0200

    Improve ls -h

--- src/busybox/disk.rs
+++ src/busybox/disk.rs
@@ -461,8 +461,9 @@ pub fn ls(sh: &mut Shell, args: Arguments) -> Result<()> {
         .arg(Arg::with_name("path")
             .multiple(true)
         )
-        .arg(Arg::with_name("long").short("l"))
-        .arg(Arg::with_name("a").short("a"))
+        .arg(Arg::with_name("long").short("l").help("Show more infos"))
+        .arg(Arg::with_name("a").short("a").help("Dummy option"))
+        .arg(Arg::with_name("h").short("h").help("Dummy option"))
         .get_matches_from_safe(args)?;
 
     let long = matches.occurrences_of("long") > 0;
