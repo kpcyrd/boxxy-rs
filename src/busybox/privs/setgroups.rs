80a0ab6942ef4785693eea633200c7eaf87bbfff
kpcyrd <git@rxv.cc>
2018-06-06 01:12:12 +0200

    Allow using an empty list for setgroups

--- src/busybox/privs/setgroups.rs
+++ src/busybox/privs/setgroups.rs
@@ -12,15 +12,17 @@ pub fn setgroups(_sh: &mut Shell, args: Arguments) -> Result<()> {
         .setting(AppSettings::DisableVersion)
         .about("Call setgroups(2)")
         .arg(Arg::with_name("group")
-            .required(true)
             .multiple(true)
             .help("The groups that should be set")
         )
         .get_matches_from_safe(args)?;
 
-    let groups = matches.values_of("group").unwrap()
-        .map(|x| x.parse())
-        .collect::<result::Result<Vec<gid_t>, _>>()?;
+    let groups = match matches.values_of("group") {
+        Some(groups) => groups
+            .map(|x| x.parse())
+            .collect::<result::Result<Vec<gid_t>, _>>()?,
+        None => Vec::new(),
+    };
 
     ffi::setgroups(&groups)?;
 
