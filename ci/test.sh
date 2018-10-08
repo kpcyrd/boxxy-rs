2582d26fa91c4c97666ec1b36fb81b3ca461ab2e
kpcyrd <git@rxv.cc>
2018-10-08 23:18:46 +0200

    Release 0.8.0

--- ci/test.sh
+++ ci/test.sh
@@ -3,7 +3,7 @@ set -ex
 
 case "$TRAVIS_OS_NAME-$TARGET" in
     linux-x86_64-unknown-linux-gnu|osx-x86_64-apple-darwin)
-        cargo test --verbose
+        cargo test --release --verbose
         ;;
     *)
         # nop
