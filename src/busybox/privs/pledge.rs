cbbe397620cf84f8c46cd51e811c54ca0fc22be4
kpcyrd <git@rxv.cc>
2022-07-17 18:15:52 +0200

    Remove structopt from project

--- src/busybox/privs/pledge.rs
+++ src/busybox/privs/pledge.rs
@@ -1,19 +1,18 @@
-use pledge::pledge as pledge_rs;
-use structopt::{StructOpt, clap::AppSettings};
-
+use clap::Parser;
 use crate::{Result, Shell, Arguments};
+use pledge::pledge as pledge_rs;
 
-#[derive(Debug, StructOpt)]
-#[structopt(global_settings = &[AppSettings::ColoredHelp])]
+#[derive(Debug, Parser)]
+#[clap(name = "pledge")]
 pub struct Args {
-    #[structopt(short)]
+    #[clap(short)]
     promises: Option<String>,
-    #[structopt(short)]
+    #[clap(short)]
     exec_promises: Option<String>,
 }
 
 pub fn pledge(_sh: &mut Shell, args: Arguments) -> Result<()> {
-    let args = Args::from_iter_safe(args)?;
+    let args = Args::try_parse_from(args)?;
 
     let promises = args.promises.as_deref();
     let exec_promises = args.exec_promises.as_deref();
